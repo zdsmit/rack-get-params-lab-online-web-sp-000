9140d10a5813d017d5866a6d3b9a76da26d2888b
Zach  Smith <zdsmith001@gmail.com>
2019-10-29 17:51:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,10 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      @@cart.each do |product|
+        resp.write "#{product}\n"
+      end
     else
       resp.write "Path Not Found"
     end
