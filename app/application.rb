03cdb5b3b2b6c66f79a3ac7dbf2ed43ad14df18a
Zach  Smith <zdsmith001@gmail.com>
2019-10-29 18:09:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,7 @@ class Application
       if @@items.include?(search_term)
         @@cart << search_term
       else
-        resp.write "Sorry, we don't have that item."
+        resp.write "We don't have that item."
       end
     else
       resp.write "Path Not Found"
